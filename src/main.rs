a1595158c1d37c0319b8111d46a3e16c75cf9f60
Kay Grewe <Kay.Grewe@protonmail.com>
2023-05-22 17:57:43 +0200

    Fixed Stuff, switched to result types in preparation for better testing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use anyhow::Result;
-use std::fs; 
+use std::fs;
 use std::io;
 use std::io::Write;
 use std::process;
-
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 enum JsonTokens<'a> {
@@ -23,8 +22,8 @@ fn scan_string<'a>(content: &'a str, start: usize) -> Result<(JsonTokens, usize)
     let bytes = &mut content.as_bytes();
     while end < bytes.len() {
         match bytes.get(end) {
-            Some(b'"') => break, 
-            Some(_) =>{
+            Some(b'"') => break,
+            Some(_) => {
                 end += 1;
             }
             None => break,
@@ -32,7 +31,7 @@ fn scan_string<'a>(content: &'a str, start: usize) -> Result<(JsonTokens, usize)
     }
 
     let length = end.saturating_sub(start);
-    Ok((JsonTokens::String(&content[start..end], length), length+2))
+    Ok((JsonTokens::String(&content[start..end], length), length + 2))
 }
 
 fn scan_identifier<'a>(content: &'a str, start: usize) -> Result<(JsonTokens, usize)> {
@@ -70,14 +69,19 @@ fn scan_json(content: &str) -> Result<Vec<JsonTokens>> {
             b':' => tokens.push(JsonTokens::Colon),
             b',' => tokens.push(JsonTokens::Comma),
             b'"' => {
-                let (token, length) = scan_string(&content[current+1..], 0)?;
+                let (token, length) = scan_string(&content[current + 1..], 0)?;
                 tokens.push(token);
                 current += length;
-                continue
-            },
+                continue;
+            }
             b'_' | b'a'..=b'z' | b'A'..=b'Z' => {
+                //FIXME: There is no identifier JSON only strings, bools, numbers and nulls
+                //so this should only check if we have true,false values or null
                 let (token, length) = scan_identifier(&content, current)?;
                 tokens.push(token);
+                //TODO: Refactor this into a Lexer struct which keeps the internal
+                //state globally available for all consumers so we don't need to
+                //take the consumed length from a level above us into account.
                 current += length;
                 continue;
             }
@@ -93,6 +97,37 @@ fn scan_json(content: &str) -> Result<Vec<JsonTokens>> {
     Ok(tokens)
 }
 
+//TODO: These should take more information i.e. where did the error occured,
+//      what exactly did ruffle the parses feathers the wrong way ?
+//TODO: Name these Error Types better
+#[derive(Debug, Clone)]
+enum ParserError {
+    InvalidValueInArray,
+    InvalidSymbolInCurrentContext,
+    InvalidKey,
+    MissingSymbol,
+    InvalidValueInCurrentContext,
+    EmptyJson,
+}
+
+impl std::fmt::Display for ParserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidValueInArray => write!(f, "todo"),
+            Self::InvalidSymbolInCurrentContext => write!(f, "todo"),
+            Self::InvalidKey => write!(f, "todo"),
+            Self::MissingSymbol => write!(f, "todo"),
+            Self::InvalidValueInCurrentContext => write!(f, "todo"),
+            Self::EmptyJson => write!(f, "todo"),
+        }
+    }
+}
+
+impl std::error::Error for ParserError {
+    fn description(&self) -> &str {
+        "JSON Parser Error"
+    }
+}
 
 struct Parser<'a> {
     position: usize,
@@ -100,7 +135,7 @@ struct Parser<'a> {
 }
 
 impl<'a> Parser<'a> {
-    fn new(tokens: &'a[JsonTokens]) -> Self {
+    fn new(tokens: &'a [JsonTokens]) -> Self {
         Self {
             position: 0,
             tokens,
@@ -126,40 +161,38 @@ impl<'a> Parser<'a> {
         Some(next_token)
     }
 
-    fn parse_json_array(&mut self) {
+    fn parse_json_array(&mut self) -> Result<()> {
         dbg!(&self.tokens[self.position]);
+
         loop {
-
-            if self.match_token(JsonTokens::ClosingSquareBrace) {
-                self.advance();
-                break;
-            }
-
+            //OPENBRACE STRING|NUMBER|BOOLEAN COMMA STRING|NUMBER|BOOLEAN ... CLOSINGBRACE
             match self.advance() {
                 Some(JsonTokens::String(value, _)) => println!("Element: {}", value),
+                Some(JsonTokens::OpenCurlyBrace) => self.parse_json_object()?,
+                _ => return Err(ParserError::InvalidSymbolInCurrentContext.into()),
+            };
+
+            match self.advance() {
+                Some(JsonTokens::ClosingSquareBrace) => break,
+                Some(JsonTokens::Comma) => self.advance(),
                 _ => todo!(),
-            }
-
-            if !self.match_token(JsonTokens::Comma) {
-                break;
-            }
-
-            self.advance();
+            };
         }
+
+        Ok(())
     }
 
-    fn parse_key_value_pair(&mut self) {
+    fn parse_key_value_pair(&mut self) -> Result<()> {
         dbg!(&self.tokens[self.position]);
         loop {
             match self.advance() {
                 Some(JsonTokens::Identifier(key, _)) => println!("{}", key),
                 Some(JsonTokens::String(key, _)) => println!("{}", key),
-                _ => crash_and_burn("key needs to be a string or a identifier"),
-            }
+                _ => return Err(ParserError::InvalidKey.into()),
+            };
 
             if !self.match_token(JsonTokens::Colon) {
-                crash_and_burn("expected colon after key");
-                break;
+                return Err(ParserError::InvalidSymbolInCurrentContext.into());
             }
 
             self.advance();
@@ -167,10 +200,10 @@ impl<'a> Parser<'a> {
             match self.advance() {
                 Some(JsonTokens::Identifier(value, _)) => println!("{}", value),
                 Some(JsonTokens::String(value, _)) => println!("{}", value),
-                Some(JsonTokens::OpenCurlyBrace) => self.parse_json_object(),
-                Some(JsonTokens::OpenSquareBrace) => self.parse_json_array(),
-                _ => crash_and_burn("value needs to be a string or a identifier"),
-            }
+                Some(JsonTokens::OpenCurlyBrace) => self.parse_json_object()?,
+                Some(JsonTokens::OpenSquareBrace) => self.parse_json_array()?,
+                _ => return Err(ParserError::InvalidValueInCurrentContext.into()), 
+            };
 
             if !self.match_token(JsonTokens::Comma) {
                 break;
@@ -178,61 +211,59 @@ impl<'a> Parser<'a> {
 
             self.advance();
         }
+
+        Ok(())
     }
 
-    fn parse_json_object(&mut self) {
+    fn parse_json_object(&mut self) -> Result<()> {
         //FIXME: Potential to blow the stack if we recurse to deep !
+        //TODO: Collapse these Error, where possible into one branch
         match self.peek() {
-            Some(JsonTokens::OpenCurlyBrace) => self.parse_json_object(),
-            Some(JsonTokens::ClosingCurlyBrace) => return,
-            Some(JsonTokens::OpenSquareBrace) => todo!(),
-            Some(JsonTokens::ClosingSquareBrace) => todo!(),
-            Some(JsonTokens::Colon) => todo!(),
-            Some(JsonTokens::Comma) => todo!(),
-            Some(JsonTokens::Identifier(_, _)) => self.parse_key_value_pair(),
-            Some(JsonTokens::String(_, _)) => self.parse_key_value_pair(),
-            Some(JsonTokens::Eof) => crash_and_burn("Unclosed JSON Object"),
-            None => crash_and_burn("Expected '}' but tokenstream was empty"),
-        }
+            Some(JsonTokens::OpenCurlyBrace) => return Err(ParserError::InvalidSymbolInCurrentContext.into()),
+            Some(JsonTokens::ClosingCurlyBrace) => return Ok(()),
+            Some(JsonTokens::OpenSquareBrace) => return Err(ParserError::InvalidSymbolInCurrentContext.into()),
+            Some(JsonTokens::ClosingSquareBrace) => return Err(ParserError::InvalidSymbolInCurrentContext.into()),
+            Some(JsonTokens::Colon) => return Err(ParserError::InvalidSymbolInCurrentContext.into()),
+            Some(JsonTokens::Comma) => return Err(ParserError::InvalidSymbolInCurrentContext.into()),
+            Some(JsonTokens::Identifier(_, _)) => self.parse_key_value_pair()?,
+            Some(JsonTokens::String(_, _)) => self.parse_key_value_pair()?,
+            Some(JsonTokens::Eof) => return Err(ParserError::EmptyJson.into()),
+            None => return Err(ParserError::EmptyJson.into()),
+        };
+
+        Ok(())
     }
 
-    fn parse(&mut self) {
+    fn parse(&mut self) -> Result<()> {
         match self.advance() {
-            Some(JsonTokens::OpenCurlyBrace) => self.parse_json_object(),
-            Some(JsonTokens::ClosingCurlyBrace) => crash_and_burn("expected the start of a json object"),
-            Some(JsonTokens::OpenSquareBrace) => crash_and_burn("expected the start of a json object but got open square brace"),
-            Some(JsonTokens::ClosingSquareBrace) => crash_and_burn("expected the start of a json object but got closed square brace"),
-            Some(JsonTokens::Colon) => crash_and_burn("expected the start of a json object but found a colon"),
-            Some(JsonTokens::Comma) => crash_and_burn("expected the start of a json object but found a comma"),
-            Some(JsonTokens::Identifier(_, _)) => crash_and_burn("identifer outside json object"),
-            Some(JsonTokens::String(_, _)) => crash_and_burn("string outside of json object"),
-            Some(JsonTokens::Eof) => crash_and_burn("Empty JSON"),
+            Some(JsonTokens::OpenCurlyBrace) => self.parse_json_object()?,
+            Some(JsonTokens::ClosingCurlyBrace) => return Err(ParserError::InvalidSymbolInCurrentContext.into()), 
+            Some(JsonTokens::OpenSquareBrace) => return Err(ParserError::InvalidSymbolInCurrentContext.into()),
+            Some(JsonTokens::ClosingSquareBrace) => return Err(ParserError::InvalidSymbolInCurrentContext.into()),
+            Some(JsonTokens::Colon) => return Err(ParserError::InvalidSymbolInCurrentContext.into()),
+            Some(JsonTokens::Comma) => return Err(ParserError::InvalidSymbolInCurrentContext.into()),
+            Some(JsonTokens::Identifier(_, _)) => return Err(ParserError::InvalidValueInCurrentContext.into()),
+            Some(JsonTokens::String(_, _)) => return Err(ParserError::InvalidValueInCurrentContext.into()),
+            Some(JsonTokens::Eof) => return Err(ParserError::InvalidValueInCurrentContext.into()),
             None => todo!(),
-        }
+        };
+
+        Ok(())
     }
 }
 
-//TODO: This thingy should be replace with a Error enum and a result type on
-//      the parser methods.
-fn crash_and_burn(message: &str) {
-    let mut stderr = io::stderr();
-    let _ = stderr.lock();
-
-    write!(stderr, "ERROR: {}", message).unwrap();
-    process::exit(1);
-}
 fn main() -> Result<()> {
     let file_content = fs::read_to_string("examples/test.json")?;
 
     let mut stdout = io::stdout();
     let _ = stdout.lock();
 
-    //TODO: For now 
+    //TODO: For now
     let lexed_json = scan_json(&file_content)?;
     //parse_json(&lexed_json);
 
     let mut parser = Parser::new(&lexed_json);
-    parser.parse();
+    parser.parse()?;
 
     if lexed_json.first() == Some(&JsonTokens::Eof) {
         let mut stderr = io::stderr();
@@ -245,4 +276,12 @@ fn main() -> Result<()> {
     write!(stdout, "{}", &file_content)?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+    #[test]
+    fn test_json_malformed_array() {
+    }
 }
